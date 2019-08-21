0f5e0381fe1a8f6ff3ac86fcdc0e566714c5bc38
Brandon Forys <bforys@gmail.com>
2019-08-20 17:55:05 -0700

    Create backend for getting playlists

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import register_user, login_user, add_code, setup_success
+from .views import register_user, login_user, add_code, setup_success, get_playlists
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('add-code', add_code),
     path('setup-success', setup_success),
+    path('get-playlists', get_playlists),
 ]
